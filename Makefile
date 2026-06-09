38f976ab5178d09c76db9910b62739c0202c562d
Aurore Rakotoarivony <aurrakot@fu-r2-p6.s19.be>
2026-06-09 11:36:40 +0200

    listing all files

--- Makefile
+++ Makefile
@@ -7,22 +7,23 @@ VENV_DIR    = .venv
 UV_CACHE    = .cache/uv-cache
 
 install:
-        $(MANAGER) sync
+	$(MANAGER) sync
 
 run:
-        $(MANAGER) run $(EXEC) main.py
+	clear
+	$(MANAGER) run $(EXEC) main.py
 
 debug:
-        $(MANAGER) run $(EXEC) -m pdb -m $(PACKAGE)
+	$(MANAGER) run $(EXEC) -m pdb -m $(PACKAGE)
 
 clean:
-        rm -rf $(VENV_DIR)
-        rm -rf $(UV_CACHE)
-        rm -rf .pytest_cache
-        rm -rf __pycache__
+	rm -rf $(VENV_DIR)
+	rm -rf $(UV_CACHE)
+	rm -rf .pytest_cache
+	rm -rf __pycache__
 
 lint:
-        $(MANAGER) run flake8 .
-        $(MANAGER) run mypy . --warn-return-any --warn-unused-ignores --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs
+	$(MANAGER) run flake8 .
+	$(MANAGER) run mypy . --warn-return-any --warn-unused-ignores --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs
 
 .PHONY: install run debug clean lint
--- main.py
+++ main.py
@@ -15,6 +15,26 @@
 
 
 """
+Step 1: Read all database
+    - Print all valid files
+    - Print the content of some of the files
+
+Step 2: Make the database usable
+    are all files of the same value?
+    do I keep the information of path?
+    - Chunk the files in 2000 char strings
+    - Upgrade chunking
+        - don't cut words
+        - avoid cutting paragraphs
+        - chunk by context?
+    - Indexing
+        - check multi-layers indexing
+
+Step 3: See later
+"""
+
+
+"""
 • Build an indexed knowledge base from the project attached files.
     - Implement a robust ingestion pipeline to process various file types (e.g., .txt, .py).
 
@@ -27,22 +47,43 @@
 """
 
 from pathlib import Path
+import os
 
 def ingest():
     pass
 
 
-def retrieve_docs():
-    """Go through all the documents and separate them into chunks of 2000 characters. Then, classify the chunks using a retrieval method (TF-IDF or BM25)."""
-    directory = Path("vllm-0.10.1")
-    with open("data.txt", "r") as f:
-        data = f.read()
+def retrieve_docs_listdir():
+    directory = "vllm-0.10.1"
+    lst_dir = os.listdir(directory)
+    # print(f"Files and dirs in {directory}:\n{lst_dir}")
+    for f in lst_dir:
+        if os.path.isfile(directory+'/'+f) and (f.endswith(".txt") or f.endswith(".md")):
+            print(f"file: {f}")
+        # if os.path.isdir(directory+'/'+f):
+        #     print(f"dir: {f}")
+
+
+def retrieve_docs_walk():
+    directory = "vllm-0.10.1"
+    lst_dir = os.listdir(directory)
+
+    lst = []
+    for _, dirs, files in os.walk(directory):
+        # print(files)
+        for f in files:
+            if f.endswith(".txt") or f.endswith(".md"):
+                print(f"file: {f}")
+                # print(dirs)
 
 
 
 def main():
-    pass
+    # retrieve_docs_listdir()
+    retrieve_docs_walk()
 
 
 if __name__ == "__main__":
+    print("--- Before main ---\n")
     main()
+    print("\n--- After  main ---")
